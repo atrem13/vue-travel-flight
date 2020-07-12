f82500c6aeb7570eff9dbb92eec3d51b27d3860f
atrem13 <mertayoga17.my@gmail.com>
2020-07-12 13:56:05 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -240,7 +240,219 @@
     </v-container>
 
     <!-- special flight deal -->
+    <v-container class="mb-5">
+      <v-sheet
+        class="mx-auto"
+        elevation="8"
+        max-width="100%"
+        outlined
+      >
+        <v-slide-group
+          v-model="model"
+          class="pa-4"
+          active-class="success"
+          show-arrows
+        >
+          <v-slide-item
+            v-for="n in 15"
+            :key="n"
+            v-slot:default="{ active, toggle }"
+          >
+            <v-card
+              :color="active ? 'grey lighten-5' : 'white'"
+              class="ma-4"
+              width="250"
+              @click="toggle"
+            >
+              <v-img
+                class="white--text align-end"
+                height="150px"
+                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+              >
+                <!-- <v-card-title>Top 10 Australian beaches</v-card-title> -->
+              </v-img>
 
+              <v-card-text class="text--primary">
+                <div class="mb-3">
+                  <v-icon left color="yellow">flight_takeoff</v-icon>
+                  Kuwait  
+                </div>
+                <div class="mb-3">
+                  <v-icon left color="yellow">flight_land</v-icon>
+                  Dubai 
+                </div>
+                <v-divider></v-divider>
+                <div class="mt-3">
+                  Starting from KWD 26.50
+                </div>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="blue"
+                  text
+                >
+                  Detail
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-slide-item>
+        </v-slide-group>
+      </v-sheet>
+    </v-container> 
+
+    <!-- get best deal banner -->
+    <v-container fluid class="ma-0 pa-0">
+      <v-parallax
+        src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col class="text-center" cols="5">
+            <h1 class="text-h4 font-weight-bold mb-4">Get the best deals</h1>
+            <h4 class="subheading grey--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quos eum rem totam iusto facere, amet eius inventore. </h4>
+            <p class="text-body-1 mt-5 font-weight-bold">100% price transparant | Trusted and free</p>
+            <v-btn 
+              class="mt-5"
+              color="warning"
+              rounded>
+              Book Now
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-parallax>
+    </v-container>
+
+    <!-- special hot deal -->
+    <v-container class="mt-5">
+      <v-layout row wrap>
+        <v-flex xs10 offset-1>
+          <v-layout row wrap>
+            <v-flex xs3 class="pa-5" v-for="(n, i) in 4" :key="i">
+              <v-card
+                  class=""
+                >
+                  <v-img
+                    class="white--text align-end"
+                    height="150px"
+                    src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                  >
+                    <!-- <v-card-title>Top 10 Australian beaches</v-card-title> -->
+                  </v-img>
+
+                  <v-card-text>
+                    <p class="">
+                      Rove Dubai Maria
+                    </p>
+                    <h3>Dubai</h3>
+                    <p>Starting from</p>
+                    <p>KWD 45.50</p>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-btn
+                      text
+                      color="primary"
+                    >
+                    See Detail
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+    <!-- subscribe to news letter -->
+     <v-container>
+       <v-row wrap>
+         <v-col xs="10" offset="1">
+            <v-row wrap>
+              <v-col
+                xs="5"
+                md="5"
+                sm="5"
+              >
+                <h3>
+                  Subscribe <br>
+                  To Our NewsLetter
+                </h3>
+
+              </v-col>
+              <v-col
+                xs="6"
+                sm="6"
+                md="6"
+              >
+                <v-text-field 
+                  type="text" 
+                  label="Email" 
+                  prepend-icon="email"  
+                  v-model="email" 
+                >
+                </v-text-field>
+                
+              </v-col>
+            </v-row>
+         </v-col>
+       </v-row>
+     </v-container>
+
+     <!-- footer -->
+      <v-container>
+         <v-row>
+           <v-col 
+            xs="12"
+            sm="4"
+            md="4"
+           >
+            <h3>About Us</h3>
+            <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni delectus officia molestiae esse sit corporis nisi consectetur, sint doloribus architecto suscipit neque sunt. Id rem, quaerat corporis ex harum blanditiis!</p>
+            <h4 class="mt-3">+62 8741 2589 633</h4>
+            <v-divider></v-divider>
+            
+           </v-col>
+           <v-spacer></v-spacer>
+           <v-col
+             xs="12"
+             sm="5"
+             md="5"
+           >
+            <v-row wrap>
+               <v-col 
+                xs="4"
+                sm="4"
+                md="4"
+                v-for="(n, i) in 3" :key="i"
+               >
+                  <p class="ma-0 pa-0">Judul</p>
+                  <p class="ma-0 pa-0">itemitem item</p>
+                  <p class="ma-0 pa-0">itemitem item</p>
+                  <p class="ma-0 pa-0">itemitem item</p>
+                  <p class="ma-0 pa-0">itemitem item</p>
+                  <p class="ma-0 pa-0">itemitem item</p>
+               </v-col>
+               <v-col 
+                xs="4"
+                sm="4"
+                md=""
+               >
+
+               </v-col>
+               <v-col 
+                xs="4"
+                sm="4"
+                md=""
+               >
+
+               </v-col>
+            </v-row>
+           </v-col>
+         </v-row>
+       </v-container>
   </v-app>
 </template>
 
@@ -252,6 +464,8 @@
     },
     data: () => ({
       drawer: null,
+      email:null,
+      flightDeals: null,
       items: [
         { icon: 'mdi-trending-up', text: 'Most Popular' },
         { icon: 'mdi-youtube-subscription', text: 'Subscriptions' },
